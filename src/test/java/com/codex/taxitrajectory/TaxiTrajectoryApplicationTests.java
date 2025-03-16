1c30f74f7d44509b5ad008b520eb2f29d5ba14b0
Hoyinl2333 <1974562160@qq.com>
2025-03-16 20:17:51 +0800

    init framwork

---
+++ src/test/java/com/codex/taxitrajectory/TaxiTrajectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codex.taxitrajectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiTrajectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
