0a5ea1eec84f394fd47b2ace85c9362e59c5e92a
Ringphone666 <610828739@qq.com>
2025-04-14 14:26:46 +0800

    Featuref/f4 (#16)
    * f4 前后对接

--- src/main/java/com/codex/taxitrajectory/controller/TaxiController.java
+++ src/main/java/com/codex/taxitrajectory/controller/TaxiController.java
@@ -1,5 +1,6 @@
 package com.codex.taxitrajectory.controller;
 
+import com.codex.taxitrajectory.model.GridCell;
 import com.codex.taxitrajectory.model.TaxiRecord;
 import com.codex.taxitrajectory.repository.DataLoader;
 import com.codex.taxitrajectory.service.TrafficAnalysisService;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/taxi")
@@ -44,7 +46,28 @@ public class TaxiController {
 
         int taxiCount = trafficAnalysisService.countTaxisInRegion(
                 start, end, topLeftLongitude, topLeftLatitude, bottomRightLongitude, bottomRightLatitude);
-
+        System.out.println(taxiCount);
         return ResponseEntity.ok(taxiCount);
+    }
+
+    @GetMapping("/densityAnalysis")
+    public ResponseEntity<Map<GridCell, Integer>> densityAnalysis(
+            @RequestParam String startTime,
+            @RequestParam String endTime,
+            @RequestParam double topLeftLongitude,
+            @RequestParam double topLeftLatitude,
+            @RequestParam double bottomRightLongitude,
+            @RequestParam double bottomRightLatitude,
+            @RequestParam double gridRadius) {
+
+        // 修改日期时间格式化器
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+        LocalDateTime start = LocalDateTime.parse(startTime, formatter);
+        LocalDateTime end = LocalDateTime.parse(endTime, formatter);
+
+        Map<GridCell, Integer> densityResult = trafficAnalysisService.analyzeTrafficDensity(
+                start, end, gridRadius);
+
+        return ResponseEntity.ok(densityResult);
     }
 }
